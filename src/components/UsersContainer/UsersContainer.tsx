c23950837ec378568cf474f509efb3203ee78a7f
AlexKapic <alexkapic@gmail.com>
2021-12-14 12:24:07 +0200

    add spinner

--- src/components/UsersContainer/UsersContainer.tsx
+++ src/components/UsersContainer/UsersContainer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { IUser } from '../../common/interfaces';
+import { LoadSpinner } from '../common/Spinner';
 import { UserCard } from './UserCard';
 
 type Props = {
@@ -9,15 +10,18 @@ type Props = {
 
 export const UsersContainer: React.FC<Props> = ({ users }) => (
   <Container className="mt-4">
-    <Row>
-      {/* if need 5 columns in row -- use "<Col key={idx} style={{ 'flex': '1 0 20%' }}>" */}
-      {users &&
-        users.map((user, idx) => (
+    {!users ? (
+      <LoadSpinner />
+    ) : (
+      <Row>
+        {/* if need 5 columns in row -- use "<Col key={idx} style={{ 'flex': '1 0 20%' }}>" */}
+        {users.map((user, idx) => (
           // eslint-disable-next-line
           <Col key={idx} lg={3} md={4} sm={6}>
             <UserCard user={user} />
           </Col>
         ))}
-    </Row>
+      </Row>
+    )}
   </Container>
 );
